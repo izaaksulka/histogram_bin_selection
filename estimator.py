97fd612d88dac7b6258e75ae00d191e86aa9e1c2
izaaksulka <izaaksulka@mines.edu>
2018-10-11 01:49:32 -0600

    seems to work on the first file (triangle)

--- estimator.py
+++ estimator.py
@@ -14,7 +14,26 @@ print (len(values))
 mink = math.log(len(values), 2)
 maxk = math.sqrt(len(values))
 
+mean_values = statistics.mean(values)
 
-for k in range(mink, maxk):
+costs = {}
+
+for k in range(int(mink), int(maxk) + 1):
     delta = len(values) / k
     
+    hist = []
+    for v in values:
+        index = int(v / delta)
+        hist.append(index * delta + delta / 2)
+
+    mean_hist = statistics.mean(hist)
+    
+    cost = abs(mean_hist - mean_values)
+    costs[k] = cost
+    
+top_three = []
+for i in range(3):
+    top_three.append(min(costs.keys(), key=lambda x: costs[x]))
+    del costs[top_three[-1]]
+    
+print (top_three)
